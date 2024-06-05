c1d457ff16a73c1a6ce23c0948bd483e602f32db
chrhuettner <chrhuettner@edu.aau.at>
2024-06-05 18:04:08 +0200

    Added NetworkConnection.
    This connection is supposed to be used by both client and host

---
+++ backend/app/src/main/java/backend/server/NetworkConnection.java
@@ -0,0 +1,233 @@
+package backend.server;
+
+import static backend.server.Constants.LOG_NETWORK;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.ArrayDeque;
+
+
+public class NetworkConnection extends Thread { //execute each instance within a separate thread
+
+    //properties needed
+    private Socket socket;
+    private BufferedWriter writer;
+    private BufferedReader reader;
+
+    private String lastMsgReceived;
+
+    private static final String TAG = LOG_NETWORK + "-NC";
+
+    private boolean isRunning;
+
+    private final Object runningToken;
+    private final ArrayDeque<String> outputBuffer;
+
+    private String ip;
+    private int port;
+    private int timeout;
+
+    private int clientId;
+    private boolean closeHasBeenRequested;
+
+    private Object closeRequestToken = false;
+
+    private boolean isServerConnection;
+
+
+    public NetworkConnection(String ip, int port, int timeout) {
+        this.isRunning = true;
+        runningToken = "";
+        outputBuffer = new ArrayDeque<>();
+
+        this.ip = ip;
+        this.port = port;
+        this.timeout = timeout;
+        closeHasBeenRequested = false;
+        closeRequestToken = "";
+    }
+
+    public NetworkConnection(Socket socket) {
+        this.isRunning = true;
+        runningToken = "";
+        outputBuffer = new ArrayDeque<>();
+
+        this.socket = socket;
+        closeHasBeenRequested = false;
+        closeRequestToken = "";
+    }
+
+    @Override
+    public void run() {
+        try {
+            if (socket == null) {
+                socket = new Socket();
+                socket.connect(new InetSocketAddress(ip, port), timeout);
+            }
+            reader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            writer = new BufferedWriter(new OutputStreamWriter(this.socket.getOutputStream()));
+
+            if (Config.role == Config.ROLE.CLIENT) {
+                String clientID = reader.readLine();
+                Log.i(Constants.LOG_MAIN, clientID);
+                Config.clientID = Integer.parseInt(clientID.split(":")[1]);
+                this.clientId = Config.clientID;
+
+                if (Config.clientID == -1) {
+                    //TODO: Notify UI that the server rejected the client (server is full)
+                    return;
+                }
+                //TODO: Notify server that this Client has joined the lobby
+
+            } else {
+                //Host always has ID 1
+                Config.clientID = 1;
+            }
+
+            Log.d(TAG, "Waiting for incoming messages");
+            while (true) {
+                read:
+                if (reader.ready()) {
+
+                    if (hasCloseBeenRequested()) {
+                        Log.e(TAG, "CONNECTION COULD NOT READ, CLOSE HAS ALREADY BEEN REQUESTED");
+                        break read;
+                    }
+
+                    String msg = reader.readLine();
+                    Log.d(TAG, "Incoming message " + msg);
+                    this.lastMsgReceived = msg;
+                    //TODO: Interpret message with a Logic class
+                }
+
+                sendThroughNetwork();
+
+                synchronized (runningToken) {
+                    if (!isRunning) {
+                        break;
+                    }
+                }
+
+            }
+
+            synchronized (closeRequestToken) {
+                if (hasCloseBeenRequested()) {
+                    synchronized (outputBuffer) {
+                        //Allow remaining messages to be sent to the server before closing the connection
+                        if (outputBuffer.isEmpty()) {
+                            synchronized (runningToken) {
+                                isRunning = false;
+                            }
+                        }
+                    }
+                }
+            }
+
+            Thread.sleep(1);
+        } catch (IOException e) {
+            Log.e(TAG, e.getMessage());
+
+        } catch (InterruptedException e) {
+            Log.e(TAG, "Interrupted!" + e);
+
+            Thread.currentThread().interrupt();
+        } finally {
+            try {
+                this.shutdown();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        synchronized (outputBuffer) {
+            if (!outputBuffer.isEmpty()) {
+                for (String message : outputBuffer) {
+                    Log.e(TAG, "CONNECTION COULDNT SEND " + message + ". CONNECTION IS ALREADY CLOSED!");
+                }
+            }
+        }
+    }
+
+    public void send(String message) {
+        if (hasCloseBeenRequested()) {
+            Log.e(TAG, "CONNECTION COULD NOT SEND " + message + ". CONNECTION HAS ALREADY BEEN REQUESTED TO CLOSE!");
+            return;
+        }
+
+        synchronized (outputBuffer) {
+
+            outputBuffer.add(message);
+
+        }
+
+    }
+
+    private void sendThroughNetwork() {
+        synchronized (outputBuffer) {
+            while (!outputBuffer.isEmpty()) {
+                try {
+                    String message = outputBuffer.pop();
+                    Log.d(TAG, "Sending following message to server: " + message);
+                    writer.write(message);
+                    writer.newLine();
+                    writer.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    public String getLastMsgReceived() {
+        if (this.lastMsgReceived == null) {
+            return "";
+        }
+        return this.lastMsgReceived;
+    }
+
+    public void close() {
+        synchronized (closeRequestToken) {
+            closeHasBeenRequested = true;
+        }
+
+    }
+
+    public boolean hasCloseBeenRequested() {
+        synchronized (closeRequestToken) {
+            return closeHasBeenRequested;
+        }
+    }
+
+    private void shutdown() throws IOException {
+        if (this.reader != null) {
+            this.reader.close();
+        }
+        if (this.writer != null) {
+            this.writer.close();
+        }
+        if (this.socket != null) {
+            this.socket.close();
+        }
+        synchronized (runningToken) {
+            isRunning = false;
+        }
+    }
+    public int getClientId() {
+        return clientId;
+    }
+
+    public boolean isServerConnection() {
+        return isServerConnection;
+    }
+
+    public void setServerConnection(boolean serverConnection) {
+        isServerConnection = serverConnection;
+    }
+}
